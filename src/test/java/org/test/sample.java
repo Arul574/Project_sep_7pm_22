6ed79f9446ca7c751d29e0c13d8969a0fbe9c426
ARUL574 <arulsai616@gmail.com>
2022-09-07 09:14:07 +0530

    premika

--- src/test/java/org/test/sample.java
+++ src/test/java/org/test/sample.java
@@ -2,6 +2,9 @@ package org.test;
 
 public class sample {
 	public static void main(String[] args) {
+		System.out.println("test 1");
+		System.out.println("test 3");
+		System.out.println("test 4");
 		
 	}
 
